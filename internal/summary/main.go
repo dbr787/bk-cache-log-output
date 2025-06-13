d8391962b48ab31bea851ef6422fc6ffbe257d7c
Daniel Ring <dbr787@gmail.com>
2025-06-13 12:57:45 +1000

    fix: correct go-pretty row usage

--- internal/summary/main.go
+++ internal/summary/main.go
@@ -52,16 +52,15 @@ func main() {
     green := color.New(color.FgGreen).SprintFunc()
     red := color.New(color.FgRed).SprintFunc()
 
-    headers := []string{"STEP", "DURATION", "CACHE HIT", "SIZE", "TRANSFER SPEED", "FILES", "CACHE REGISTRY"}
-    t.AppendHeader(table.Row(headers...), headerColors)
+    t.AppendHeader(table.Row{"STEP", "DURATION", "CACHE HIT", "SIZE", "TRANSFER SPEED", "FILES", "CACHE REGISTRY"}, headerColors)
 
     for _, s := range steps {
         hit := red("❌")
         if s.CacheHit {
             hit = green("✅")
         }
-        values := []string{s.Step, s.Duration, fmt.Sprintf("%v", s.CacheHit), s.Size, s.TransferSpeed, fmt.Sprintf("%d", s.Files), s.CacheRegistry}
-        t.AppendRow(table.Row(values...))
+        row := table.Row{s.Step, s.Duration, hit, s.Size, s.TransferSpeed, s.Files, s.CacheRegistry}
+        t.AppendRow(row)
     }
 
     fmt.Println(t.Render())
